51247c29cab5aed2446c5b3b21a3dd7e55bf2e00
CNFloss <JSdeveloperColin@gmail.com>
2020-09-23 04:02:47 -0400

    folder heirarchy established

---
+++ assets/config/config.js
@@ -0,0 +1,17 @@
+import { Physics } from "phaser";
+
+export default {
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 640,
+    height: 512,
+    pixelArt: true,
+    roundPixels: true,
+    physics: {
+      default: "arcade",
+      arcade: {
+        debug: true,
+        gravity: { y:0 }
+      }
+    }
+  };
